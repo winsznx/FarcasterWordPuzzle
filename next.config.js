e7b2c28c9d1e66a9797b021026b44dfdfd81d3f6
winsznx <timjosh507@gmail.com>
2025-11-14 01:00:56 +0100

    chore: add TypeScript and Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    return config;
+  },
+  images: {
+    domains: ['localhost'],
+  },
+};
+
+module.exports = nextConfig;
